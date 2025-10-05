b38f1b8fa0010bd3aaf51e4c6a7ee28af977c1ee
Matheus Todao <todaomatheus@hotmail.com>
2025-10-05 10:56:52 -0300

    feat(app): add CategoriesModule to the main application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
   controllers: [AppController],
   providers: [
     AppService,
