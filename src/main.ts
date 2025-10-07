5ca19f34613f2be9579534995107c86365165a7d
Matheus Todao <todaomatheus@hotmail.com>
2025-10-07 19:33:09 -0300

    feat(swagger): add Swagger configuration and setup in main application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { FincheckSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swagger = new FincheckSwagger(SwaggerModule);
+
+  swagger.setup(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
